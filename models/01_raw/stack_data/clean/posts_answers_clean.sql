6dae82606d265955831e8020f0cea5a403bedacd
Gethin Jones <gethinj37@gmail.com>
2025-01-26 20:16:35 +0000

    updated answers clean

--- models/01_raw/stack_data/clean/posts_answers_clean.sql
+++ models/01_raw/stack_data/clean/posts_answers_clean.sql
@@ -6,24 +6,22 @@
     )
 }}
 select  
-id,
-title,
-body,
-accepted_answer_id,
-answer_count,
+id                          as answer_id,
+title                       as answer_title,
+body                        as answer_body,
 comment_count,
 community_owned_date,
-creation_date,
+creation_date               as answer_creation_date,
 favorite_count,
-last_activity_date,
-last_edit_date,
-last_editor_display_name,
-last_editor_user_id,
-owner_display_name,
-owner_user_id,
-parent_id,
+last_activity_date          as answer_last_activity_date, 
+last_edit_date              as answer_ast_edit_date,
+last_editor_display_name    as answer_last_editor_display_name,
+last_editor_user_id         as answer_last_editor_user_id,
+owner_display_name          as answer_owner_display_name,
+owner_user_id               as answer_owner_user_id,
+parent_id as question_id,
 post_type_id,
 score,
 tags,
 view_count
-from rittman-analytics-trial-gj.stack_data.posts_answers
+from {{source("stack_data","posts_answers")}}
--- models/02_pl_stage/fact_questions_stage.sql
+++ models/02_pl_stage/fact_questions_stage.sql
@@ -8,13 +8,32 @@
 
 
 SELECT
-  accepted_answer_id,
-  creation_date,
-  id,
-  last_activity_date,
-  last_editor_user_id,
-  owner_user_id,
-  tags,
-  view_count
+  q.id,
+  q.title,
+  q.body,
+  q.accepted_answer_id,
+  q.creation_date,
+  q.last_activity_date,
+  q.last_editor_user_id,
+  q.owner_user_id,
+  q.tags,
+  q.view_count
+  a.answer_body,
+  a.answer_last_activity_date, 
+  a.answer_ast_edit_date,
+  a.answer_last_editor_display_name,
+  a.answer_last_editor_user_id,
+  a.answer_owner_display_name,
+  a.answer_owner_user_id,
+
 FROM
-  {{ref('posts_questions_clean')}} 
+  {{ref('posts_questions_clean')}} q
+left join
+  {{ref('dim_users')}} uq
+  on q.owner_user_id = uq.id
+left join
+  {{ref('dim_users')}} ua
+  on a.answer_owner_user_id = ua.id
+left join
+  {{ref('posts_answers_clean')}} a
+  on q.id = a.question_id
