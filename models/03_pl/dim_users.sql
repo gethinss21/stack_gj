d3d5999e414def618fac0abf3a2fd704f68e6543
Gethin Jones <gethinj37@gmail.com>
2025-01-26 19:35:44 +0000

    running posts_questions

--- models/03_pl/dim_users.sql
+++ models/03_pl/dim_users.sql
@@ -11,12 +11,7 @@ select
     about_me,
     age,
     creation_date,
-    last_access_date,
     location,
-    reputation,
-    up_votes,
-    down_votes,
-    views,
     profile_image_url,
     website_url
 from {{ref('dim_users_stage')}}
